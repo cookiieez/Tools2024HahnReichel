4e21d005d01838f9504ba1dfb5567f431f26bdab
Elias <66306408+cookiieez@users.noreply.github.com>
2024-05-06 15:05:34 +0200

    bugfix

--- reichel_hahn_android/main.java
+++ reichel_hahn_android/main.java
@@ -2,4 +2,8 @@ public void thisISANewMethode(){ }
 public static void main(String[] args) {
 	double d = 0;
     int i = 0;
+
+    if(i == -1){
+        System.out.println("Fehler im Code");
+    }
 }
